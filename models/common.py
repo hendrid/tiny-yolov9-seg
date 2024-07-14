6ad9aece5a06fb21795a95a177c95c4af117132f
hendrid <46194260+hendrid@users.noreply.github.com>
2024-07-14 23:23:15 +0700

    Update common.py

--- models/common.py
+++ models/common.py
@@ -419,11 +419,22 @@ class GLAM(nn.Module):
         fused_feature_maps = (all_feature_maps * weights).sum(1)
         return fused_feature_maps
 
+# class depthwise_separable_conv(nn.Module):
+#     def __init__(self, nin, nout, kernel_size = 3, padding = 1, bias=False):
+#         super(depthwise_separable_conv, self).__init__()
+#         self.depthwise = nn.Conv2d(nin, nin, kernel_size=kernel_size, padding=padding, groups=nin, bias=bias)
+#         self.pointwise = nn.Conv2d(nin, nout, kernel_size=1, bias=bias)
+
+#     def forward(self, x):
+#         out = self.depthwise(x)
+#         out = self.pointwise(out)
+#         return out
+
 class depthwise_separable_conv(nn.Module):
-    def __init__(self, nin, nout, kernel_size = 3, padding = 1, bias=False):
+    def __init__(self, nin, nout, kernel_size = 3, padding = 1, alpha = 0.75, bias=False):
         super(depthwise_separable_conv, self).__init__()
-        self.depthwise = nn.Conv2d(nin, nin, kernel_size=kernel_size, padding=padding, groups=nin, bias=bias)
-        self.pointwise = nn.Conv2d(nin, nout, kernel_size=1, bias=bias)
+        self.depthwise = nn.Conv2d(nin, int(nin * alpha), kernel_size=kernel_size, padding=padding, groups=int(nin * alpha), bias=bias)
+        self.pointwise = nn.Conv2d(int(nin * alpha), int(nout * alpha), kernel_size=1, bias=bias)
 
     def forward(self, x):
         out = self.depthwise(x)
